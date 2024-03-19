f1b959bd529f0a09a8a54578da381be5ed81a8ba
VijayWath] <126336500+VijayWath@users.noreply.github.com>
2024-03-19 22:54:04 +0530

    added JWT and optimised code

---
+++ auth.js
@@ -0,0 +1,24 @@
+import JWT from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const secrete = process.env.JWT_SECRETE;
+
+function createToken(user) {
+  const payload = {
+    _id: user._id,
+    email: user.email,
+    profileImageUrl: user.profileimageurl,
+    role: user.role,
+  };
+  const token = JWT.sign(payload, secrete);
+  return token;
+}
+
+function validateToken(token) {
+  const payload = JWT.verify(token, secrete);
+  return payload;
+}
+
+export { validateToken, createToken };
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,46 +1,62 @@
-import db from "../postgressSetup.js"
-import { createHmac } from 'node:crypto';
-import dotenv from'dotenv';
+import db from "../postgressSetup.js";
+import { createHmac } from "node:crypto";
+import dotenv from "dotenv";
+import { validateToken, createToken } from "../auth.js";
 
 dotenv.config();
 
-async function handelUserSignIn(req,res){
-   return res.render("signIn")
+async function handelUserSignIn(req, res) {
+  return res.render("signIn");
 }
 
-async function handelUserSignup(req,res){
-   return res.render("signup.ejs")
+async function handelUserSignup(req, res) {
+  return res.render("signup.ejs");
 }
 
-async function PostHandelUserSignup(req,res){
-    const { fullName,email,password } = req.body
-    const secrete = process.env.HASH_SECRETE;
-    const hashedPassword = createHmac('sha256', secrete)
-               .update(password)
-               .digest('hex');
-    await db.query("INSERT INTO users(fullName,email,password) VALUES($1,$2,$3)",[fullName,email,hashedPassword])
-    return res.redirect("/")
+async function PostHandelUserSignup(req, res) {
+  const { fullName, email, password } = req.body;
+  const secrete = process.env.HASH_SECRETE;
+  const hashedPassword = createHmac("sha256", secrete)
+    .update(password)
+    .digest("hex");
+  await db.query(
+    "INSERT INTO users(fullName,email,password) VALUES($1,$2,$3)",
+    [fullName, email, hashedPassword]
+  );
+  return res.redirect("/");
 }
 
-async function PostHandelUserSignIn(req,res){
-    const {email,password } = req.body
-    const secrete = process.env.HASH_SECRETE;
-    const hashedPassword = createHmac('sha256', secrete)
-               .update(password)
-               .digest('hex');
-  await db.query("SELECT * FROM users WHERE email = $1 AND password = $2", [email, hashedPassword],(err,res)=>{
-        if(err){
-            console.log("postgtess error")
-        }else{
-            const user = res.rows[0]
-            console.log(user)
-            if(!user){
-                return res.json({error:"email or pass is wrong"})
-            }
-        }
-    });
-    return res.redirect("/")   
-    
+async function PostHandelUserSignIn(req, res) {
+  const { email, password } = req.body;
+  const secrete = process.env.HASH_SECRETE;
+  const hashedPassword = createHmac("sha256", secrete)
+    .update(password)
+    .digest("hex");
+
+  try {
+    const result = await db.query(
+      "SELECT * FROM users WHERE email = $1 AND password = $2",
+      [email, hashedPassword]
+    );
+    const user = result.rows[0];
+
+    if (!user) {
+      return res.render("signin", { error: "incorrect pass or email" });
+    }
+
+    const token = createToken(user);
+    console.log(token);
+
+    return res.cookie("token", token).redirect("/");
+  } catch (err) {
+    console.error("postgress error", err);
+    return res.status(500).json({ error: "Internal server error" });
+  }
 }
 
-export {handelUserSignIn,handelUserSignup ,PostHandelUserSignup,PostHandelUserSignIn}
+export {
+  handelUserSignIn,
+  handelUserSignup,
+  PostHandelUserSignup,
+  PostHandelUserSignIn,
+};
